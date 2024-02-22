3c4db9b9574740bf8d04c17cf5762619ed4f9b7f
Killer66562 <jimmy688382@gmail.com>
2024-02-22 13:42:58 +0800

    is_prime

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn is_prime(number: u64) -> bool {
+    if number == 0 || number == 1 {
+        return false;
+    }
+    let mut x: u64 = 2;
+    while x * x <= number{
+        if number % x == 0 {
+            return  false;
+        }
+        x += 1;
+    }
+    return  true;
+}
+
+fn main() {
+    for x in 1u64..100u64 {
+        if is_prime(x) {
+            println!("{} is a prime.", x);
+        }
+        else {
+            println!("{} is not a prime.", x);
+        }
+    }
+}
